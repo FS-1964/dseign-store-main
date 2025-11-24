d604f4fb114992bd781019e21f17296f014a742d
FS-1964 <fariborzsaidi@hotmail.com>
2025-11-24 15:25:13 +0100

    change cta

--- src/layouts/home/Cta.jsx
+++ src/layouts/home/Cta.jsx
@@ -1,9 +1,7 @@
 const Cta = () => {
   return (
     <section className='cta h-[50vh] grid place-items-center'>
-      <p className='text-6xl h-[4.8rem] px-4 tracking-widest font-semibold bg-primary rounded-2xl select-none text-slate-200'>
-        cta
-      </p>
+     
     </section>
   );
 };
